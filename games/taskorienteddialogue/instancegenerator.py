981baa3cad38d67347ac56860136477e3d56cc4c
Cordula <cordula.c.scholz@gmail.com>
2024-07-04 14:39:53 +0200

    Set up InstanceGenerator

---
+++ games/taskorienteddialogue/instancegenerator.py
@@ -0,0 +1,61 @@
+"""Generate instances as json files for DialogueGame.
+"""
+
+import random
+import string
+
+from clemgame.clemgame import GameInstanceGenerator
+import constants
+
+
+GAME_NAME = 'dialogue'
+N_INSTANCES = 10
+SEED = 9191
+
+
+class DialogueInstanceGenerator(GameInstanceGenerator):
+    """generate() method creates JSON file with key 'experiments', list of experiments.
+
+    Args:
+        GameInstanceGenerator (_type_): _description_
+    """
+    def __init__(self):
+        super().__init__("dialoguegame")
+
+    # TODO: Specify on_generate
+    # Variations on topic, variations with same topic...
+    def on_generate(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        # topic
+        prompt_a = self.load_template('resources/initial_prompts/dummy_prompt_a_hotel')
+        prompt_b = self.load_template('resources/initial_prompts/dummy_prompt_b_hotel')
+
+        # TODO: Set up relevant params
+        for n in range(0,5):
+            experiment = self.add_experiment(n)
+
+            for game_id in range(N_INSTANCES):
+                instance = self.add_game_instance(experiment, game_id)
+                instance['prompt_player_a'] = prompt_a
+                instance['prompt_player_b'] = prompt_b
+
+    # Fill prompt template
+    # TODO: Modify!
+    def create_prompt(self,
+                      topic: str,
+                      prompt: str,
+                      letter: str,
+                      n_turns: int) -> str:
+        """Replace a prompt template with slot values."""
+        text = string.Template(prompt).substitute(topic=topic, letter=letter,
+                                                  nturns=n_turns)
+        return text
+
+
+if __name__ == '__main__':
+    random.seed(SEED)
+    DialogueInstanceGenerator().generate()
