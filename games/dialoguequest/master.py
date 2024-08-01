84fab7be8b515c03064491e75057812e699a6495
Cordula <cordula.c.scholz@gmail.com>
2024-08-01 11:40:03 +0200

    Refine master impl

--- games/dialoguequest/master.py
+++ games/dialoguequest/master.py
@@ -2,15 +2,23 @@
 Implementation of a GameMaster to control game mechanisms.
 """
 
-from typing import Dict, List
+from typing import Dict, List, Tuple
 from backends import Model
 from clemgame.clemgame import DialogueGameMaster, GameBenchmark, GameScorer, Player
 from clemgame import get_logger
-import constants
+# import constants
+# from dialoguequest import constants
 
 
 logger = get_logger(__name__)
-GAME_NAME = constants.GAME_NAME
+GAME_NAME = "dialoguequest"
+# GAME_NAME = constants.GAME_NAME
+
+
+# def print_constants(value):
+#     print(value)
+
+# print_constants(GAME_NAME)
 
 
 class Questioner(Player):
@@ -58,11 +66,14 @@ class DialogueQuest(DialogueGameMaster):
         player_models
     """
     # TODO: Check params!
+    # ??? key errors - not in instances file ???
     def __init__(self, experiment: Dict, player_models: List[Model]):
         super().__init__(GAME_NAME, experiment, player_models)
-        self.max_turns: int = experiment["max_turns"]
-        self.questioner_initial_prompt = self.experiment["prompt_a_initial_prompt"]
-        self.answerer_initial_prompt = self.experiment["prompt_b_initial_prompt"]
+        # self.max_turns: int = experiment["max_turns"]
+        self.max_turns: int = 10
+        print(f"EXPERIMENT: {experiment}")
+        # self.questioner_initial_prompt = self.experiment["prompt_player_a"]
+        # self.answerer_initial_prompt = self.experiment["prompt_player_b"]
 
     # functions:
     # - `def _on_setup(self, **kwargs)` which must be implemented. Use `add_player()` here to add the players.
@@ -71,8 +82,8 @@ class DialogueQuest(DialogueGameMaster):
         logger.info("_on_setup")
         self.game_instance = game_instance
 
-        self.questioner = Questioner(self.player_models[0], self.max_turns)
-        self.answerer = Answerer(self.player_models[1])
+        self.questioner = Questioner(self.player_models[0], "A")
+        self.answerer = Answerer(self.player_models[1], "B")
 
         self.add_player(self.questioner)
         self.add_player(self.answerer)
@@ -90,13 +101,34 @@ class DialogueQuest(DialogueGameMaster):
         """
         proceed = True if not self.current_turn >= self.max_turns else False
         return proceed
-    
-    # - `def _validate_player_response(self, player: Player, utterance: str) -> bool` to decide if an utterance should be added. This is also the place to check for game end conditions.
-    #     - `def _on_parse_response(self, player: Player, utterance: str) -> Tuple[str, bool]` to decide if a response utterance should be modified. If not simply return the utterance.
-    #         When a modified utterance and a true value is returned, then a 'parse' event is logged.
-    # - `def _after_add_player_response(self, player: Player, utterance: str)` to add the utterance to other player's history, if necessary.
-    #         To do this use the method `add_user_message(other_player,utterance)`.
+
+    # TODO: Implement + design validation / end of game!
+    # not valid: empty
+    # end of game: all slots filled
+    def _validate_player_response(self, player: Player, utterance: str) -> bool:
+        return True
+
+    # TODO: Implement + design validation
+    # json object?
+    def _on_parse_response(self, player: Player, utterance: str) -> Tuple[str, bool]:
+        return utterance, True
+
+    # TODO: Design + modify logging events!
+    def _after_add_player_response(self, player: Player, utterance: str):
+        if player == Questioner:
+            self.add_user_message(self.questioner, utterance)
+        elif player == Answerer:
+            self.add_user_message(self.answerer, utterance)
+
     # - the general game hooks `_on_before_game()` and `_on_before_game()`
+
+    # TODO: Check these general turn defs
+    def _on_before_turn(self, turn_idx: int):
+        return super()._on_before_turn(turn_idx)
+
+    def _on_after_turn(self, turn_idx: int):
+        return super()._on_after_turn(turn_idx)
+
     # - the general turn hooks `_on_before_turn(turn_idx)` and `_on_after_turn(turn_idx)`
 
     def compute_score(self, episode_interactions: Dict):
@@ -114,15 +146,29 @@ class DialogueQuestBenchmark(GameBenchmark):
     Args:
         GameBenchmark (_type_): _description_
     """
-    def __init__(self, name: str):
-        super().__init__(name)
+    def __init__(self):
+        super().__init__(GAME_NAME)
+        self.name = GAME_NAME
 
     def get_description(self) -> str:
-        return "This is a TOD-Game."
+        return "This is a Task Oriented Dialogue Game."
 
     # experiment from instances.json, player_models == dialogue pair
     def create_game_master(self, experiment: Dict, player_models: List[Model]) -> DialogueGameMaster:
+        print(f"Hello! {self.name}")
         return DialogueQuest(experiment, player_models)
 
     def is_single_player(self) -> bool:
         return False
+
+# def main():
+#     # select one instance
+#     experiments = file_utils.load_json("in/instances.json", "referencegame")
+#     instance = experiments["experiments"][0]["game_instances"][0]
+#     master = ReferenceGameMaster(instance, ["gpt-3.5-turbo", "gpt-3.5-turbo"])
+#     master.setup(**instance)
+#     master.play()
+
+
+# if __name__ == '__main__':
+#     main()
