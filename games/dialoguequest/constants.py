653a580b45c1e9e1837a963a84e2ae5d92c02ceb
Cordula <cordula.c.scholz@gmail.com>
2024-09-11 13:56:11 +0200

    Include bus in list of domains

--- games/dialoguequest/constants.py
+++ games/dialoguequest/constants.py
@@ -3,9 +3,9 @@
 
 GAME_NAME = "dialoguequest"
 MAX_TURNS = 10
-N_INSTANCES = 1
+N_INSTANCES = 2
+N_EXPERIMENTS = 2
 SEED = 4
-TOPICS = ['attraction', 'hotel', 'restaurant', 'train']
+TOPICS = ['attraction', 'bus', 'hotel', 'restaurant', 'train']
 NUMBER_OF_SLOTS = 2
-# TOPICS = ['police', 'taxi']
 WORDS_PATH = 'resources/{}_words.json'
